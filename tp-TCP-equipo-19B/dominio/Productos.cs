22c72e527a50bf077537d5a12de6476a9a2450f0
Nicovolt <166955943+Nicovolt@users.noreply.github.com>
2024-10-16 16:39:26 -0300

    inicio de clases
    da arranque las clases de la base de datos y las clases del dominio que por ahora sabemos que usaremos.

---
+++ tp-TCP-equipo-19B/dominio/Productos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Productos
+    {
+        public int Id_producto {  get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public int Id_marca { get; set; }
+        public int Id_categoria { get; set; }
+    }
+}
