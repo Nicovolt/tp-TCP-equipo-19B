59015a6cbea06828624bb423498bd59ba0b7433d
Luka <luka.sktt1@gmail.com>
2024-11-07 00:35:52 -0300

    Organizacion para marca y categoria
    Se realizo el front con 2 cards diferentes para agregar - modificar/eliminar tanto para marca como para categoria

--- tp-TCP-equipo-19B/tp-TCP-equipo-19B/Categoria.aspx.designer.cs
+++ tp-TCP-equipo-19B/tp-TCP-equipo-19B/Categoria.aspx.designer.cs
@@ -24,24 +24,6 @@ namespace tp_TCP_equipo_19B
         protected global::System.Web.UI.WebControls.TextBox inpCat;
 
         /// <summary>
-        /// Control ddlCategoria.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlCategoria;
-
-        /// <summary>
-        /// Control inpNombreCategoriaNueva.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox inpNombreCategoriaNueva;
-
-        /// <summary>
         /// Control lblError.
         /// </summary>
         /// <remarks>
@@ -60,12 +42,21 @@ namespace tp_TCP_equipo_19B
         protected global::System.Web.UI.WebControls.Label lblMensajeError;
 
         /// <summary>
-        /// Control ScriptManager1.
+        /// Control ddlCategoria.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        protected global::System.Web.UI.WebControls.DropDownList ddlCategoria;
+
+        /// <summary>
+        /// Control inpNombreCategoriaNueva.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inpNombreCategoriaNueva;
     }
 }
--- tp-TCP-equipo-19B/tp-TCP-equipo-19B/MarcaCategoria.aspx.designer.cs
+++ tp-TCP-equipo-19B/tp-TCP-equipo-19B/MarcaCategoria.aspx.designer.cs
@@ -24,6 +24,24 @@ namespace tp_TCP_equipo_19B
         protected global::System.Web.UI.WebControls.TextBox inpNombreMar;
 
         /// <summary>
+        /// Control lblError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblError;
+
+        /// <summary>
+        /// Control lblMensajeError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensajeError;
+
+        /// <summary>
         /// Control ddlMarcape.
         /// </summary>
         /// <remarks>
@@ -40,23 +58,5 @@ namespace tp_TCP_equipo_19B
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox inpNombreMarcaNueva;
-
-        /// <summary>
-        /// Control lblError.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblError;
-
-        /// <summary>
-        /// Control ScriptManager1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.ScriptManager ScriptManager1;
     }
 }
