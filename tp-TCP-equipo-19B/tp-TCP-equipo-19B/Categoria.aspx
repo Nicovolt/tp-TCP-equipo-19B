7a811bc05859b7447ba21faacbef66760075f976
Luka <luka.sktt1@gmail.com>
2024-11-06 23:59:29 -0300

    Categoria
    Se puede administrar categoria al igual que marca

---
+++ tp-TCP-equipo-19B/tp-TCP-equipo-19B/Categoria.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Categoria.aspx.cs" Inherits="tp_TCP_equipo_19B.Formulario_web12" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder" runat="server">
+
+    <section class="contenedor">
+    <div class="form-contenedor">
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Nombre Categoria</label>
+            <asp:TextBox runat="server" type="text" class="form-control" id="inpCat" placeholder="Nombre de categoria"/>
+        </div>
+
+
+        <asp:Button Text="Agregar" runat="server" Onclick="Agregar" />
+
+
+
+        <div class="mb-3">
+            <label for="ddlCategoria" class="form-label">categoria</label>
+            <asp:DropDownList runat="server" ID="ddlCategoria" CssClass="form-select">
+            </asp:DropDownList>
+        </div>
+
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Nuevo Nombre Categoria</label>
+            <asp:TextBox runat="server" type="text" class="form-control" ID="inpNombreCategoriaNueva" placeholder="Nombre de categoria" />
+        </div>
+
+
+        <asp:Button text="Modificar" runat="server" Onclick="Modificar"/>
+        <asp:Button Text="eliminar" runat="server" Onclick ="Eliminar"/>
+        <asp:Label runat="server" ID="lblError" CssClass="text-danger" />
+        <asp:Label ID="lblMensajeError" runat="server" ForeColor="Red" Visible="false"></asp:Label>
+
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+
+
+
+ 
+
+    </div>
+
+</section>
+
+</asp:Content>
---
+++ tp-TCP-equipo-19B/tp-TCP-equipo-19B/Categoria.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace tp_TCP_equipo_19B
+{
+
+
+    public partial class Formulario_web12
+    {
+
+        /// <summary>
+        /// Control inpCat.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inpCat;
+
+        /// <summary>
+        /// Control ddlCategoria.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCategoria;
+
+        /// <summary>
+        /// Control inpNombreCategoriaNueva.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox inpNombreCategoriaNueva;
+
+        /// <summary>
+        /// Control lblError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblError;
+
+        /// <summary>
+        /// Control lblMensajeError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensajeError;
+
+        /// <summary>
+        /// Control ScriptManager1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+    }
+}
